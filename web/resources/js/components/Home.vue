51f85aff22902736cc2b0fd0267b7681c11ec19a
Sajjad Aljileezi <30444995+SajjadAljileezi@users.noreply.github.com>
2021-02-14 10:54:05 -0800

    first commit

---
+++ web/resources/js/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+ <div>
+     <Nav />
+ </div>
+
+</template>
+
+<script>
+import Nav from './Nav'
+
+export default {
+    components:{
+        Nav,
+
+    },
+}
+</script>
---
+++ web/resources/js/components/Nav.vue
@@ -0,0 +1,14 @@
+<template>
+ <div>
+   <h1>nav</h1>
+ </div>
+
+</template>
+
+<script>
+
+
+export default {
+
+}
+</script>
